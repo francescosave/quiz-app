0149100272a83674537bf225d2ae3b15d5ee0501
Francesco Ricci <francescosav.ricci@libero.it>
2024-05-22 23:33:18 +0200

    feat: view correct answer only wrong  answer  select.

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react'
 import './Quiz.css'
 import { data } from '../assets/data.js'
@@ -8,6 +8,12 @@ const Quiz = () => {
   let [index, setIndex] = useState(0);
   let [question, setQuestion] = useState(data[index]);
   let [lock, setLock] = useState(false);
+  let option1 = useRef(null);
+  let option2 = useRef(null);
+  let option3 = useRef(null);
+  let option4 = useRef(null);
+  let option_array = [option1,option2,option3,option4];
+
 
   const ceckAns = (e, ans) => {
 
@@ -18,12 +24,11 @@ const Quiz = () => {
       } else {
         e.target.classList.add("wrong")
         setLock(true);
+        option_array[question.ans - 1].current.classList.add('correct');
       }
 
 
     }
-
-
   }
 
   return (
@@ -32,13 +37,13 @@ const Quiz = () => {
       <hr />
       <h2>{index + 1}.{question.question}</h2>
       <ul>
-        <li onClick={(e) => { ceckAns(e, 1) }}>{question.option1}</li>
-        <li onClick={(e) => { ceckAns(e, 2) }}>{question.option2}</li>
-        <li onClick={(e) => { ceckAns(e, 3) }}>{question.option3}</li>
-        <li onClick={(e) => { ceckAns(e, 4) }}>{question.option4}</li>
+        <li ref={option1} onClick={(e) => { ceckAns(e, 1) }}>{question.option1}</li>
+        <li ref={option2} onClick={(e) => { ceckAns(e, 2) }}>{question.option2}</li>
+        <li ref={option3} onClick={(e) => { ceckAns(e, 3) }}>{question.option3}</li>
+        <li ref={option4} onClick={(e) => { ceckAns(e, 4) }}>{question.option4}</li>
       </ul>
       <button>Next</button>
-      <div className="index">1 of 5 questions</div>
+      <div className="index">{index + 1} of {data.length} questions</div>
     </div>
   )
 }
